dfe6c621eb88697a3dd6c0f41b35a17cb8090898
Brandon Herrera Cerdas <brand@Brandon>
2023-02-28 21:04:26 -0600

    clase semana 07 - bootstrap y controller (crear, eliminar)

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonaController {
@@ -30,8 +33,29 @@ public class PersonaController {
     @GetMapping("/personaN")
     public String crearPersona(Model model){
         List<Pais> listaPaises = paisService.listCountry();
-        model.addAttribute("titulo",new Persona());
-        model.addAttribute("personas",listaPaises);
+        model.addAttribute("persona",new Persona());//"persona"-> como lo leemos en el html //envia el objeto al html
+        model.addAttribute("paises",listaPaises);
+        return "crear";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/persona";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePersona(persona);
+        return "redirect:/persona";
+    }
+    
+    @GetMapping("/editPersona/{id}")
+    public String editarPersona(@PathVariable("id") Long idPersona, Model model){
+        Persona persona = personaService.getPersonaById(idPersona);
+        List<Pais> listaPaises = paisService.listCountry();
+        model.addAttribute("persona",persona);
+        model.addAttribute("paises",listaPaises);
         return "crear";
     }
 }
